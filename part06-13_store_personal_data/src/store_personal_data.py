c94b0f89b1cf814dd7af590d841c0f0861239b02
MUKUND <mukundsaroj.it@gmail.com>
2026-06-23 21:35:40 +0530

    another one

--- part06-13_store_personal_data/src/store_personal_data.py
+++ part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def store_personal_data(person: tuple):
+    person = list(person)
+    with open("people.csv", "a") as people:
+        person[1] = str(person[1])
+        person[2] = str(person[2])
+        line = ";".join(person)
+        people.write(line + "\n")
+
+if __name__ == "__main__":
+    store_personal_data(("Paul Paulson", 37, 175.5))
