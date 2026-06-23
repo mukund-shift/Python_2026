e87a98c06541219b2d672cd157204c3c40d6422b
MUKUND <mukundsaroj.it@gmail.com>
2026-06-23 21:27:26 +0530

    more solving

--- part06-11_diary/src/diary.py
+++ part06-11_diary/src/diary.py
@@ -1,1 +1,22 @@
 # Write your solution here
+
+while True:
+    print("1 - add an entry, 2 - read entries, 0 - quit")
+    operation = input("Function: ")
+
+    if operation == "1":
+        entry = input("Diary entry: ")
+        with open("diary.txt", "a") as diary:
+            diary.write(f"{entry}\n")
+        print("Diary saved")
+
+    elif operation == "2":
+        print("Entries:")
+        with open("diary.txt") as diary:
+            for line in diary:
+                line = line.strip()
+                print(line)
+
+    elif operation == "0":
+        print("Bye now!")
+        break
--- part06-12_filtering_file_contents/src/filtering_file_contents.py
+++ part06-12_filtering_file_contents/src/filtering_file_contents.py
@@ -1,1 +1,32 @@
 # Write your solution here
+
+def exp_solver(exp: str):
+    if "+" in exp:
+        index = exp.index("+")
+        return int(exp[:index]) + int(exp[index:])
+    else:
+        index = exp.index("-")
+        return int(exp[:index]) - int(exp[index + 1:])
+
+def filter_solutions():
+    open("correct.csv", "w").close()
+    open("incorrect.csv", "w").close()
+    with open("solutions.csv") as solutions:
+        for line in solutions:
+            line = line.strip()
+            temp = line.split(";")
+            print(temp)
+            corr = exp_solver(temp[1])
+            print(corr)
+            if int(temp[2]) == corr:
+                with open("correct.csv", "a") as correct:
+                    correct.write(";".join(temp) + "\n")
+                    print("Added to correct")
+            else:
+                with open("incorrect.csv", "a") as incorrect:
+                    incorrect.write(";".join(temp) + "\n")
+                    print("Added to incorrect")
+
+
+if __name__ == "__main__":
+    filter_solutions()
