f5f0866b8d68941004426ae5f4171604d3e2e6b3
MUKUND <mukundsaroj.it@gmail.com>
2026-06-25 10:25:12 +0530

    +1

--- part06-18_parameter_validation/src/parameter_validation.py
+++ part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,14 @@
 # Write your solution here
+
+def check(name: str, age: int):
+    if name == "" or len(name) > 40:
+        raise ValueError("Enter proper values stupid")
+    name = name.split(" ")
+    if len(name) < 2:
+        raise ValueError("Enter proper values stupid")
+    if age < 0 or age > 150:
+        raise ValueError("Enter proper values stupid")
+    
+def new_person(name: str, age: int):
+    check(name, age)
+    return (name, age)
--- part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
+++ part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
@@ -1,1 +1,30 @@
 # Write your solution here
+
+
+def check(week_no: str, winning_nos: list):
+    try:
+        week_no = int(week_no)
+        if len(winning_nos) != len(set(winning_nos)) or len(winning_nos) != 7:
+            raise ValueError
+        for number in winning_nos:
+            number = int(number)
+            if number < 1 or number > 39:
+                raise ValueError
+        return True
+    except ValueError:
+        return False
+    
+
+def filter_incorrect():
+    with open("lottery_numbers.csv") as file:
+        with open("correct_numbers.csv", "w") as correct:
+            for line in file:
+                line = line.strip()
+                index = line.index(";")
+                week_no = line[5:index]                     # 1
+                winning_nos = line[index + 1:].split(",")   # 2
+                if check(week_no, winning_nos):
+                    correct.write(line + "\n")
+
+if __name__ == "__main__":
+    filter_incorrect()
