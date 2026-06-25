cb4f03980b30d8a30faedc172def41da787bcaba
MUKUND <mukundsaroj.it@gmail.com>
2026-06-25 09:47:39 +0530

    +1
    this one too easy

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def read_input(text, lb, ub):
+    while True:
+        try:
+            number = int(input(text))
+            if lb <= number <= ub:
+                return number
+        except ValueError:
+            pass
+        print(f"You must type in an integer between {lb} and {ub}")
+
+if __name__ == "__main__":
+    number = read_input("Please type in a number: ", 5, 10)
+    print("You typed in:", number)
