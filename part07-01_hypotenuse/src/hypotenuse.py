0b878725488a7939e048d9d74f193c855d5f5c96
MUKUND <mukundsaroj.it@gmail.com>
2026-06-26 10:40:23 +0530

    +2

--- part07-01_hypotenuse/src/hypotenuse.py
+++ part07-01_hypotenuse/src/hypotenuse.py
@@ -1,1 +1,9 @@
 # Write your solution here
+
+def hypotenuse(leg1: float, leg2: float):
+    from math import sqrt
+    hypt = sqrt(leg1**2 + leg2**2)
+    return hypt
+
+if __name__ == "__main__":
+    print(hypotenuse(5,12))
--- part07-02_special_characters/src/special_characters.py
+++ part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,22 @@
 # Write your solution here
+
+import string
+
+
+def separate_characters(my_string: str):
+    asci_set = set(string.ascii_letters)
+    punc_set = set(string.punctuation)
+    asci = ""
+    punc = ""
+    rest = ""
+    for letter in my_string:
+        if letter in asci_set:
+            asci += letter
+        elif letter in punc_set:
+            punc += letter
+        else:
+            rest += letter
+    return asci, punc, rest
+
+if __name__ == "__main__":
+    print(separate_characters("Hi!, I like doing exercises in Python."))
