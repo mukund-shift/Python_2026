6c2b846241e2a74454d3a08fe5ed40b19662f708
MUKUND <mukundsaroj.it@gmail.com>
2026-06-26 11:57:09 +0530

    +1
    personal random pass generator

--- part07-06_password_generator_part_2/src/password_generator_part_2.py
+++ part07-06_password_generator_part_2/src/password_generator_part_2.py
@@ -1,1 +1,32 @@
 # Write your solution here
+import random
+
+def generate_strong_password(length: int, numbers: bool, special: bool):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    nums = "0123456789"
+    specials = "!?=+-()#"
+    result = ""
+    chars = [letters]
+
+    if numbers:
+        chars.append(nums)
+    if special:
+        chars.append(specials)
+    letter_char_present = False
+
+    for i in range(length):
+        choice = random.choice(chars)
+        if choice == letters:
+            letter_char_present = True
+        char = choice[random.randint(0, len(choice) - 1)]
+        result += char
+    if not letter_char_present:
+        result = list(result)
+        result[random.randint(0, len(result) - 1)] = letters[random.randint(0, 25)]
+        result = str(result)
+    
+    return result
+
+if __name__ == "__main__":
+    pw = generate_strong_password(2, False, False)
+    print(pw)
