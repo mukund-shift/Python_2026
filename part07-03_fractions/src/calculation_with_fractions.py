a1d22fc12faf3609b93c8915972c5319c822d4ba
MUKUND <mukundsaroj.it@gmail.com>
2026-06-26 11:05:27 +0530

    +1
    fractions module:
    Fractions is a class:
    Object format: Fraction(1,5) #1/5
    
    print(Fraction(1,5) ----> 1/5

--- part07-03_fractions/src/calculation_with_fractions.py
+++ part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+import fractions
+
+def fractionate(amount: int):
+    return [fractions.Fraction(1, amount)] * amount
+
+
+if __name__ == "__main__":
+    op = fractionate(10)
+    for i in range(len(op)):
+        print(op[i])
+    print(op)
