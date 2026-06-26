112cbf8c8fa284241390ff15b8a73d235bc8929f
MUKUND <mukundsaroj.it@gmail.com>
2026-06-26 11:19:09 +0530

    +1
    Checked out randint, shuffle, sample, choice

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,14 @@
 # Write your solution here
+
+import random
+
+
+def lottery_numbers(amount: int, lower: int, upper: int):
+    pool = list(range(lower, upper + 1))
+    sample = random.sample(pool, amount)
+    sample.sort()
+    return sample
+
+if __name__ ==  "__main__":
+    example = lottery_numbers(25, 100, 999)
+    print(example)
