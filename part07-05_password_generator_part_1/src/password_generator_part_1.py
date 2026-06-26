58b63c015d995a3818afbc7c519a07cfeccc85f8
MUKUND <mukundsaroj.it@gmail.com>
2026-06-26 11:30:31 +0530

    +1

--- part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+import random
+
+def generate_password(length: int):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    result = ""
+    for i in range(length):
+        char = letters[random.randint(0, 25)]
+        result += char
+    return result
