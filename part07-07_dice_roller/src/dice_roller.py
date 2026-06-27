a24b771c76ad7a0136a9b0e5925053cebb2bb392
MUKUND <mukundsaroj.it@gmail.com>
2026-06-27 11:00:35 +0530

    +3
    Transitive dies are interesting

--- part07-07_dice_roller/src/dice_roller.py
+++ part07-07_dice_roller/src/dice_roller.py
@@ -1,1 +1,40 @@
 # Write your solution here
+import random
+
+def roll(dice: str):
+    die = { "a" : [3, 3, 3, 3, 3, 6],
+            "b" : [2, 2, 2, 5, 5, 5],
+            "c" : [1, 4, 4, 4, 4, 4]}
+    dice = dice.lower()
+    return random.choice(die[dice])
+
+
+def play(die1: str, die2: str, times: int):
+    wins1 = 0
+    wins2 = 0
+    ties = 0
+    for i in range(times):
+        temp1 = roll(die1)
+        temp2 = roll(die2)
+        if temp1 > temp2:
+            wins1 += 1
+        elif temp2 > temp1:
+            wins2 += 1
+        else:
+            ties += 1
+    return wins1, wins2, ties
+
+if __name__ == "__main__":
+    
+    result = play("A", "B", 1000)
+    print(result)
+    result = play("B", "C", 1000)
+    print(result)
+    result = play("A", "C", 1000)
+    print(result)
+    result = play("A", "A", 1000)
+    print(result)
+    result = play("B", "B", 1000)
+    print(result)
+    result = play("C", "C", 1000)
+    print(result)
--- part07-08_random_words/src/random_words.py
+++ part07-08_random_words/src/random_words.py
@@ -1,1 +1,22 @@
 # Write your solution here
+import random
+
+all_words = set()
+
+with open("words.txt") as words:
+    for line in words:
+        line = line.strip()
+        all_words.add(line)
+
+def words(n: int, beginning: str):
+    matches = []
+    for word in all_words:
+        if word.startswith(beginning):
+            matches.append(word)
+    if len(matches) < n:
+        raise ValueError
+    return random.sample(matches, n)
+
+if __name__ == "__main__":
+    word_list = words(4, "bl")
+    print(word_list)
