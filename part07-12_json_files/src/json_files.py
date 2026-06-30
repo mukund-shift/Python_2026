e143834d16cf4a22724dbeb7e5e26ce938c717e4
MUKUND <mukundsaroj.it@gmail.com>
2026-06-30 23:46:36 +0530

    +1
    Moving to reading data from urls

--- part07-12_json_files/src/json_files.py
+++ part07-12_json_files/src/json_files.py
@@ -1,1 +1,21 @@
 # Write your solution here
+import json
+
+def print_persons(filename: str):
+    with open(filename) as file:
+        data = file.read()
+        data = json.loads(data)
+    for item in data:
+        # item == dicts
+        print(item["name"], end = " ")
+        print(f"{item["age"]} years", end = " (")
+        sublist = item["hobbies"]
+        for i in range(len(sublist)):
+            print(sublist[i], end = "")
+            if i <= len(sublist) - 2:
+                print(", ", end = "")
+        else:
+            print(")")
+
+if __name__ == "__main__":
+    print_persons("file4.json")
--- part07-13_course_statistics/src/course_statistics.py
+++ part07-13_course_statistics/src/course_statistics.py
@@ -1,1 +1,4 @@
-# Write your solution here
+import urllib.request
+
+my_request = urllib.request.urlopen("https://studies.cs.helsinki.fi/stats-mock/api/courses")
+print(my_request.read())
