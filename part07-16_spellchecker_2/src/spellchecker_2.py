516a382c7c28632e0664c6c7911d6117ed9f4c4f
MUKUND <mukundsaroj.it@gmail.com>
2026-07-03 17:21:44 +0530

    +1

--- part07-16_spellchecker_2/src/spellchecker_2.py
+++ part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,31 @@
 # Write your solution here
+from difflib import get_close_matches
+text = input("Write text: ")
+
+words = text.split(" ")
+correct_words = []
+suggestions = {}
+
+with open("wordlist.txt") as reference:
+    for line in reference:
+        line = line.strip()
+        correct_words.append(line)
+
+correct_words = set(correct_words)
+
+
+for i in range(len(words)):
+    if words[i].lower() not in correct_words:
+        words[i] = f"*{words[i]}*"
+        suggestions[words[i][1:-1]] = get_close_matches(words[i][1:-1], correct_words)
+
+print(" ".join(words))
+print("suggestions:")
+for key, value in suggestions.items():
+    print(f"{key}: ", end = "")
+    op_string = ""
+    for item in value:
+        op_string += f"{item}, "
+    else:
+        op_string = op_string[:-2]
+    print(op_string)
--- part07-17_string_helper/src/string_helper.py
+++ part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,31 @@
 # Write your solution here
+
+def change_case(orig_string: str):
+    uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowers = "abcdefghijklmnopqrstuvwxyz"
+    temp = list(orig_string)
+    for i in range(len(temp)):
+        if temp[i] in uppers:
+            temp[i] = temp[i].lower()
+        elif temp[i] in lowers:
+            temp[i] = temp[i].upper()
+
+    return "".join(temp)
+
+def split_in_half(orig_string: str):
+    length = len(orig_string)
+    mid = length // 2
+    return orig_string[:mid], orig_string[mid:]
+
+def remove_special_characters(orig_string: str):
+    non_special = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "
+    temp = list(orig_string)
+    for i in range(len(temp)):
+        if temp[i] not in non_special:
+            temp[i] = ""
+    return "".join(temp)
+
+if __name__ == "__main__":
+    print(change_case("Well hello there!"))
+    print(split_in_half("Well hello there!"))
+    print(remove_special_characters("Well hello there!"))
